958a57bbae8d9d9db4f142bb8f39657b825171ba
Espinet François <dev.espinetfrancois@gmail.com>
2012-09-30 11:45:31 +0200

    Ajout d'un autoloader (autoload.php) a inclure dans toutes les pages Correction d'une parenthèse dasn DemandeManager.class.php

---
+++ classes/autoload.php
@@ -0,0 +1,12 @@
+<?php
+function autoloader($class) {
+	try {
+		include($class.".class.php");
+	} catch(Exception $e) {
+		throw new Exception("Erreur lors du chargement de la classe : ".$class);
+	}
+
+}
+
+
+spl_autoload_register('autoloader');
