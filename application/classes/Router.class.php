6df407756084983da6a67443b941036ce8d206d7
Espinet François <dev.espinetfrancois@gmail.com>
2012-10-16 11:51:00 +0200

    commentage de la classe router

--- application/classes/Router.class.php
+++ application/classes/Router.class.php
@@ -47,6 +47,7 @@ class Router {
      * @var Layout
      */
     protected $layout = null;
+
     /**
      * Constructeur, prend en argument l'url demandée ($_SERVER['REQUEST_URI'])
      * @author francois.espinet
@@ -62,14 +63,14 @@ class Router {
 
         //si la requete est invalide
         if ($this->requete->is_invalide) {
+            //on retourne l'accueil et page non trouvée
             $this->layout->not_found = true;
             $this->setAccueil();
+            return;
         }
 
         //si la requete est valide, on met en marche le mécanisme de routage.
         $this->_setFileFromUrl();
-
-        //die($this->file);
     }
 
     /**
@@ -78,7 +79,9 @@ class Router {
      */
     private function _loadIni($IniFile) {
         if ( ($urls = parse_ini_file($IniFile, true)) ) {
+            //tableau des prefixes de l'application , correspond aux sections du fichier ini
             $this->prefixes = $urls['prefixes'];
+            //tableau des url, contient suffixe de la route => fichier à charger
             $this->urls = array(self::SECTION_ROOT => $urls[self::SECTION_ROOT]);
             foreach ($this->prefixes as $keypref => $prefixe ) {
                 $this->urls[$prefixe] = $urls[$keypref];
@@ -88,6 +91,10 @@ class Router {
         }
     }
 
+    /**
+     * Methode qui prevoit l'affichage de la page d'accueil
+     * @author francois.espinet
+     */
     private function setAccueil() {
         $this->file = PAGES_PATH.'/'.$this->urls[self::SECTION_ROOT]['accueil'];
         return;
@@ -99,39 +106,61 @@ class Router {
      * @author francois.espinet
      */
     private function _setFileFromUrl() {
+        //on commence par mettre le chemin vers les pages
         $this->file = PAGES_PATH.'/';
+        //si le prefix n'est pas null (url de la forme /prefix/suffixe)
         if ($this->requete->prefixe != null) {
             $prefix = $this->__traitementPrefixe();
+            //si le prefix existe
             if (! $this->layout->not_found) {
+                //on traite le suffixe avec le prefixe fournit
                 $this->__traitementSuffixe($prefix);
             }
         } else {
+            //on traite directement le suffixe
             $this->__traitementSuffixe();
         }
-
+        //si la page n'est pas trouvée, on met l'accueil
         if ($this->layout->not_found) {
             $this->setAccueil();
         }
-            
-    }
 
+    }
+    /**
+     * Traitement du préfixe de la requete
+     * @author francois.espinet
+     * @return prefixe de la requete ou null si le prefix n'existe pas dans la base
+     */
     private function __traitementPrefixe() {
         if (array_key_exists($this->requete->prefixe, $this->urls)) {
+            //on retourne le prefix s'il existe
             return $this->requete->prefixe;
         } else {
+            //on signale au layout que la page est non-trouvée
             $this->layout->not_found = true;
+            return null;
         }
     }
 
+    /**
+     * Traitement de la partie suffixe de la requête
+     * @author francois.espinet
+     * @param string $prefix les prefixe précédement calculé (peut-être nul!)
+     */
     private function __traitementSuffixe($prefix = self::SECTION_ROOT) {
-        if (array_key_exists($this->requete->suffixe, $this->urls[$prefix]) ) {
+        if ($this->requete->suffixe == null) {
+            // si le suffixe est nul, on renvoie l'accueil
+            $this->setAccueil();
+        } elseif (array_key_exists($this->requete->suffixe, $this->urls[$prefix]) ) {
+            //on ajout le fichier signifié dans l'ini au fichier à charger
             $this->file .= $this->urls[$prefix][$this->requete->suffixe];
+            //si on est dans l'administration
             if ($this->requete->prefixe == 'administration') {
+                //on ajoute le menu d'administration
                 $this->layout->is_admin = true;
             }
-        } elseif ($this->requete->suffixe == null) {
-            $this->setAccueil();
         } else {
+            //sinon, on signale que la page est inconnue
             $this->layout->not_found = true;
         }
     }
