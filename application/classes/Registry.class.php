49172382c3cb46cf1cc55c7de006f576f7fc78d5
Espinet François <dev.espinetfrancois@gmail.com>
2013-03-09 19:56:55 +0100

    Mise en place d'un registre - permet a terme de sauvegarder des instances de connexion de BDD - permet de mettre d'autres objets dont l'utilisation doit/peut être unique sur l'application

---
+++ application/classes/Registry.class.php
@@ -0,0 +1,106 @@
+<?php
+
+class Registry extends ArrayObject
+{
+    /**
+     * Class name of the singleton registry object.
+     * @var string
+     */
+    private static $_registryClassName = 'Registry';
+
+    /**
+     * Registry object provides storage for shared objects.
+     * @var Registry
+     */
+    private static $_registry = null;
+
+    /**
+     * Retrieves the default registry instance.
+     *
+     * @return Registry
+     */
+    public static function getInstance()
+    {
+        if (self::$_registry === null) {
+            self::$_registry = new Registry();
+        }
+
+        return self::$_registry;
+    }
+
+    /**
+     *
+     * @author francois.espinet
+     * @param mixed $index
+     * @throws Zend_Exception
+     * @return mixed
+     */
+    public static function get($index)
+    {
+        $instance = self::getInstance();
+
+        if (!$instance->offsetExists($index)) {
+            throw new Exception("No entry is registered for key ".$index);
+        }
+
+        return $instance->offsetGet($index);
+    }
+
+    /**
+     * setter method, basically same as offsetSet().
+     *
+     * This method can be called from an object of type Zend_Registry, or it
+     * can be called statically.  In the latter case, it uses the default
+     * static instance stored in the class.
+     *
+     * @param string $index The location in the ArrayObject in which to store
+     *   the value.
+     * @param mixed $value The object to store in the ArrayObject.
+     * @return void
+     */
+    public static function set($index, $value)
+    {
+        $instance = self::getInstance();
+        $instance->offsetSet($index, $value);
+    }
+
+    /**
+     * Returns TRUE if the $index is a named value in the registry,
+     * or FALSE if $index was not found in the registry.
+     *
+     * @param  string $index
+     * @return boolean
+     */
+    public static function isRegistered($index)
+    {
+        if (self::$_registry === null) {
+            return false;
+        }
+        return self::$_registry->offsetExists($index);
+    }
+
+    /**
+     * Constructs a parent ArrayObject with default
+     * ARRAY_AS_PROPS to allow acces as an object
+     *
+     * @param array $array data array
+     * @param integer $flags ArrayObject flags
+     */
+    public function __construct($array = array(), $flags = parent::ARRAY_AS_PROPS)
+    {
+        parent::__construct($array, $flags);
+    }
+
+    /**
+     * @param string $index
+     * @returns mixed
+     *
+     * Workaround for http://bugs.php.net/bug.php?id=40442 (ZF-960).
+     */
+    public function offsetExists($index)
+    {
+        return array_key_exists($index, $this);
+    }
+
+}
+
