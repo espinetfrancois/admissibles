fd32e856b89469921350016206ded4ba5341155e
Espinet François <dev.espinetfrancois@gmail.com>
2013-04-03 14:36:24 +0200

    suppression de la possibilité de créer un dépot par php (marche pas)
    --HG--
    branch : prod

--- updates/index.php
+++ updates/index.php
@@ -8,32 +8,33 @@ require_once(UPDATES_PATH.'/PHPGit/Repository.php');
 try {
     $repo = new PHPGit_Repository(ROOT_PATH);
 } catch (InvalidGitRepositoryDirectoryException $e) {
-    //le dépot n'est pas initialisé
-    try {
-        define('TEMP_DIR_PATH', UPDATES_PATH.'/tempdir');
-        mkdir(TEMP_DIR_PATH);
-        //initialisation du dépot
-//         shell_exec("cd ".TEMP_DIR_PATH." && git init");
-        $repo = PHPGit_Repository::create(TEMP_DIR_PATH);
-        $conf = $repo->getConfiguration();
-        //ajout du proxy
-        shell_exec("cd ".TEMP_DIR_PATH);
-        $conf->set("http.proxy", "http://kuzh.polytechnique.fr:8080/");
-        //url du repo
-        $repo->git("remote add origin https://github.com/espinetfrancois/admissibles.git");
-        //récupération de tout
-        $repo->git("fetch --all");
-        //placement sur la branche production
-        $repo->git("pull --rebase origin production");
+    die("Erreur lors de l'initialisation de la mise à jour : ".$e);
+//     //le dépot n'est pas initialisé
+//     try {
+//         define('TEMP_DIR_PATH', UPDATES_PATH.'/tempdir');
+//         mkdir(TEMP_DIR_PATH);
+//         //initialisation du dépot
+// //         shell_exec("cd ".TEMP_DIR_PATH." && git init");
+//         $repo = PHPGit_Repository::create(TEMP_DIR_PATH);
+//         $conf = $repo->getConfiguration();
+//         //ajout du proxy
+//         shell_exec("cd ".TEMP_DIR_PATH);
+//         $conf->set("http.proxy", "http://kuzh.polytechnique.fr:8080/");
+//         //url du repo
+//         $repo->git("remote add origin https://github.com/espinetfrancois/admissibles.git");
+//         //récupération de tout
+//         $repo->git("fetch --all");
+//         //placement sur la branche production
+//         $repo->git("pull --rebase origin production");
 
-        //déplacement des fichiers dans le bon dossier
-        shell_exec("cd ".ROOT_PATH." && cp -rf ".TEMP_DIR_PATH."/* ".TEMP_DIR_PATH."/.git .");
+//         //déplacement des fichiers dans le bon dossier
+//         shell_exec("cd ".ROOT_PATH." && cp -rf ".TEMP_DIR_PATH."/* ".TEMP_DIR_PATH."/.git .");
 
-        //suppression du dossier
-        unlink(TEMP_DIR_PATH);
-    } catch (GitRuntimeException $e) {
-        echo "Une erreur est survenue lors de l'execution du clone : <br/>".$e->getMessage().'<br/>';
-    }
+//         //suppression du dossier
+//         unlink(TEMP_DIR_PATH);
+//     } catch (GitRuntimeException $e) {
+//         echo "Une erreur est survenue lors de l'execution du clone : <br/>".$e->getMessage().'<br/>';
+//     }
 }
 
 try {
