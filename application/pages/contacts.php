45f427d5c4a814682766645a237e08f17bce2f9e
Espinet François <dev.espinetfrancois@gmail.com>
2013-04-10 21:25:27 +0200

    Remplissage de la page contacts

--- application/pages/contacts.php
+++ application/pages/contacts.php
@@ -1,8 +1,4 @@
-<?php
-/**
- * Page pour les contacts
- */
-// require_once(APPLICATION_PATH.'/inc/sql.php');
-
-
-echo "<h2>Contacts</h2>";
+<h2>Contacts</h2>
+<p>Pour contacter les administrateurs de ce site, envoyez un mail à <a href="mailto:admissibles@binets.polytechnique.fr">admissibles@binets.polytechnique.fr</a>, nous vous répondrons dans les plus brefs délais.</p>
+<p>Pour des informations plus générales sur l'École Polytechnique, <a href="http://www.polytechnique.edu/accueil/bas-de-page/contacts/" target="_blank">visitez la page suivante</a>.</p>
+<p>Pour contacter la Direction des concours : <a href="http://www.admission.polytechnique.edu/accueil/concours-cycle-polytechnicien/" target="_blank">c'est ici</a>.</p>
