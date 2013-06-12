d10c9c6626ab7586de7ebc49f5c0f4b00f6af329
Espinet François <dev.espinetfrancois@gmail.com>
2013-06-12 10:02:09 +0200

    Ajout d'un fonctionnalité pour connaitre les disponibilités des élèves par série

---
+++ application/pages/admin/inscriptionsx.php
@@ -0,0 +1,31 @@
+<?php
+require_once(APPLICATION_PATH.'/inc/fkz_auth.php');
+$parametres = Registry::get('parametres');
+$db = Registry::get('db');
+
+// Identification
+if (! (isset($_SESSION['administrateur']) && $_SESSION['administrateur'] === true)) {
+	frankiz_do_auth('/administration/gestion');
+	return;
+}
+
+//identification ok, affichage de l'interface
+echo '<h2>Interface d\'administration</h2>';
+echo '<h3>Disponibilités des X</h3>';
+echo '<span id="page_id">48</span>';
+
+$series = $parametres->getXInSeries();
+
+
+foreach ($series as $serie => $aX) {
+    $sTable = '<table class="dispo-series"><thead><tr><th>'.$serie.'</th></tr></thead><tbody>';
+    if (count($aX) == 0) {
+        $sTable .= '<tr><td>Aucun élève disponible</td></tr>';
+    }
+    foreach ($aX as $x) {
+        $sTable .= '<tr><td>'.$x.'</td></tr>';
+    }
+    $sTable .= '</tbody>';
+    $sTable .= '</tfoot><tr><th>Nombre d\'élèves diponibles : '.count($aX).'</tr></th></tfoot></table>';
+    echo $sTable;
+}
